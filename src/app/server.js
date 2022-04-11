c04de70f5623155ebf5e9e3049e75f4b4a54cc21
sitraka <shajaniri@gmail.com>
2022-04-11 19:28:11 +0300

    g

---
+++ src/app/server.js
@@ -0,0 +1,505 @@
+// Use Express
+var express = require("express");
+// Use body-parser
+var bodyParser = require("body-parser");
+// Use MongoDB
+var mongodb = require("mongodb");
+var ObjectID = mongodb.ObjectID;
+// The database variable
+var database;
+
+//sha1
+var sha1 = require('sha1');
+
+var path = require("path");
+
+//cors
+var cors = require('cors');
+
+//mail
+var nodemailer = require('nodemailer');
+const { async } = require("rxjs/internal/scheduler/async");
+const { getMaxListeners } = require("process");
+const { createFalse } = require("typescript");
+
+// The products collection
+var PRODUCTS_COLLECTION = "products";
+
+// Create new instance of the express server
+var app = express();
+
+// Define the JSON parser as a default way 
+// to consume and produce data through the 
+// exposed APIs
+app.use(cors({ origin: "*" }));
+app.use(bodyParser.json());
+
+// Create link to Angular build directory
+// The ng build command will save the result
+// under the dist folder.
+var distDir = __dirname + "/dist/node-express-angular/";
+app.use(express.static(distDir));
+
+// Local database URI.
+const LOCAL_DATABASE = "mongodb+srv://sisi:Sitraka1@cluster0.1phmo.mongodb.net/E-kaly?retryWrites=true&w=majority";
+
+// Local port.
+const LOCAL_PORT = 8080;
+
+// Init the server
+mongodb.MongoClient.connect(process.env.MONGODB_URI || LOCAL_DATABASE, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+}, function(error, client) {
+
+    // Check if there are any problems with the connection to MongoDB database.
+    if (error) {
+        console.log(error);
+        process.exit(1);
+    }
+
+    // Save database object from the callback for reuse.
+    database = client.db('E-kaly');
+    console.log("Database connection done.");
+
+    //const db = client.db('star-wars-result2')
+    //const resultCollection = db.collection('result')
+
+    // Initialize the app.
+    var server = app.listen(process.env.PORT || LOCAL_PORT, function() {
+        var port = server.address().port;
+        console.log("App now running on port", port);
+    });
+});
+
+app.get('/login/:email', (req, res) => res.sendFile(path.join(__dirname, '/dist/node-express-angular/index.html')));
+app.get('/login', (req, res) => res.sendFile(path.join(__dirname, '/dist/node-express-angular/index.html')));
+app.get('/plat/nouveau', (req, res) => res.sendFile(path.join(__dirname, '/dist/node-express-angular/index.html')));
+app.get('/restaurant', (req, res) => res.sendFile(path.join(__dirname, '/dist/node-express-angular/index.html')));
+app.get('/inscription', (req, res) => res.sendFile(path.join(__dirname, '/dist/node-express-angular/index.html')));
+app.get('/admin', (req, res) => res.sendFile(path.join(__dirname, '/dist/node-express-angular/index.html')));
+app.get('/client', (req, res) => res.sendFile(path.join(__dirname, '/dist/node-express-angular/index.html')));
+app.get('/commande/:id', (req, res) => res.sendFile(path.join(__dirname, '/dist/node-express-angular/index.html')));
+app.get('/commande-en-cours', (req, res) => res.sendFile(path.join(__dirname, '/dist/node-express-angular/index.html')));
+app.get('/restaurant/commande', (req, res) => res.sendFile(path.join(__dirname, '/dist/node-express-angular/index.html')));
+app.get('/livreur', (req, res) => res.sendFile(path.join(__dirname, '/dist/node-express-angular/index.html')));
+app.get('/admin/benefice', (req, res) => res.sendFile(path.join(__dirname, '/dist/node-express-angular/index.html')));
+app.get('/restaurant/benefice', (req, res) => res.sendFile(path.join(__dirname, '/dist/node-express-angular/index.html')));
+
+/*  "/api/status"
+ *   GET: Get server status
+ *   PS: it's just an example, not mandatory
+ */
+app.get("/api/status", function(req, res) {
+    res.status(200).json({ status: "UP" });
+});
+
+/*  "/api/products"
+ *  GET: finds all products
+ */
+app.get("/api/products", function(req, res) {
+    database.collection('products').find({}).toArray(function(error, data) {
+        if (error) {
+            manageError(res, err.message, "Failed to get contacts.");
+        } else {
+            res.status(200).json(data);
+        }
+    });
+});
+
+/*  "/api/products"
+ *   POST: creates a new product
+ */
+app.post("/api/products", function(req, res) {
+    var product = req.body;
+
+    if (!product.name) {
+        manageError(res, "Invalid product input", "Name is mandatory.", 400);
+    } else if (!product.brand) {
+        manageError(res, "Invalid product input", "Brand is mandatory.", 400);
+    } else {
+        database.collection('products').insertOne(product, function(err, doc) {
+            if (err) {
+                manageError(res, err.message, "Failed to create new product.");
+            } else {
+                res.status(201).json(doc.ops[0]);
+            }
+        });
+    }
+});
+
+/*  "/api/products/:id"
+ *   DELETE: deletes product by id
+ */
+app.delete("/api/products/:id", function(req, res) {
+    if (req.params.id.length > 24 || req.params.id.length < 24) {
+        manageError(res, "Invalid product id", "ID must be a single String of 12 bytes or a string of 24 hex characters.", 400);
+    } else {
+        database.collection(PRODUCTS_COLLECTION).deleteOne({ _id: new ObjectID(req.params.id) }, function(err, result) {
+            if (err) {
+                manageError(res, err.message, "Failed to delete product.");
+            } else {
+                res.status(200).json(req.params.id);
+            }
+        });
+    }
+});
+
+//Get all resto 
+app.get("/api/user/resto", function(req, res) {
+    database.collection('user').find({ profil: "resto" }).toArray(function(error, data) {
+        if (error) {
+            manageError(res, err.message, "Failed to get contacts.");
+        } else {
+            res.status(200).json(data);
+        }
+    });
+});
+
+//Get all livreur 
+app.get("/api/user/livreur", function(req, res) {
+    database.collection('user').find({ profil: "livreur" }).toArray(function(error, data) {
+        if (error) {
+            manageError(res, err.message, "Failed to get contacts.");
+        } else {
+            res.status(200).json(data);
+        }
+    });
+});
+
+//Get all commande 
+app.get("/api/commande/all", function(req, res) {
+    database.collection('commande').find({ livrer: false }).toArray(function(error, data) {
+        if (error) {
+            manageError(res, err.message, "Failed to get contacts.");
+        } else {
+            res.status(200).json(data);
+        }
+    });
+});
+
+
+//nouveaux plats
+app.post("/api/plat", function(req, res) {
+    var plat = req.body;
+    database.collection('plat').insertOne(plat, function(err, doc) {
+        if (err) {
+            manageError(res, err.message, "Failed to create new plat.");
+        } else {
+            res.status(201).json(doc.ops[0]);
+        }
+    });
+});
+
+//Get all plat by resto 
+app.get("/api/plat/:idResto", function(req, res) {
+    var id = req.params.idResto;
+    var ObjectId = require('mongodb').ObjectID;
+    database.collection('plat').find({ idResto: id ,  visible: true }).toArray(function(error, data) {
+        if (error) {
+            manageError(res, err.message, "Failed to get contacts.");
+        } else {
+            res.status(200).json(data);
+        }
+    });
+});
+
+//Get resto by id 
+app.get('/api/user/resto/:id', (req, res) => {
+    var id = req.params.id;
+    var ObjectID = require('mongodb').ObjectID;
+    database.collection('user').findOne({ _id:new ObjectID(id)})
+        .then(result => {
+            res.status(200).json(result);
+        });
+});
+
+//Commander
+app.post("/api/commande", function(req, res) {
+    var commande = req.body;
+    commande.date=Date.now();
+    database.collection('commande').insertOne(commande, function(err, doc) {
+        if (err) {
+            manageError(res, err.message, "Failed to create new account.");
+        } else {
+            res.status(201).json(doc.ops[0]);
+        }
+    });
+});
+
+//Assigner livreur
+app.get("/api/commande/assigner/:id/:livreur",function(req, res) {
+    var ObjectId = require('mongodb').ObjectID;
+    database.collection('commande').findOneAndUpdate({ _id: new ObjectID(req.params.id) }, { $set: { livreur : req.params.livreur } }), (function(error, data) {
+        if (error) {
+            manageError(res, err.message, "Failed to get contacts.");
+        } else {
+            res.status(200).json({ "message": "compte valider" });
+        }
+    });
+});
+
+//Benefice par plat
+app.get("/api/commande/benefice/:id", function(req, res) {
+    var id = req.params.id;
+    var ObjectID = require('mongodb').ObjectID;
+    database.collection('commande').aggregate([
+        {
+            $match: { idResto: id }
+        },
+        {
+           $group: { _id: "$nom", quantite: { $sum: "$quantite"}, totalRevient: { $sum: "$prixRevient"}, totalPrix: { $sum: "$prixUnitaire"}}
+        }
+    ]).toArray(function(error, data) {
+        if (error) {
+            manageError(res, err.message, "Failed to get contacts.");
+        } else {
+            res.status(200).json(data);
+        }
+    });
+});
+
+//Benefice par resto
+app.get("/api/commande/beneficeResto", function(req, res) {
+    var ObjectID = require('mongodb').ObjectID;
+    database.collection('commande').aggregate([
+        {
+           $group: { _id: "$idResto", quantite: { $sum: "$quantite"}, totalRevient: { $sum: "$prixRevient"}, totalPrix: { $sum: "$prixUnitaire"}}
+        }
+    ]).toArray(function(error, data) {
+        if (error) {
+            manageError(res, err.message, "Failed to get contacts.");
+        } else {
+            res.status(200).json(data);
+        }
+    });
+});
+
+//Get commande en cours client
+app.get("/api/commande/:idClient", function(req, res) {
+    var id = req.params.idClient;
+    var ObjectId = require('mongodb').ObjectID;
+    database.collection('commande').find({ idClient: id ,  livrer: false }).toArray(function(error, data) {
+        if (error) {
+            manageError(res, err.message, "Failed to get contacts.");
+        } else {
+            res.status(200).json(data);
+        }
+    });
+});
+
+//Get commande en cours restaurant
+app.get("/api/restaurant/commande/:idResto", function(req, res) {
+    var id = req.params.idResto;
+    var ObjectId = require('mongodb').ObjectID;
+    database.collection('commande').find({ idResto: id ,  livrer: false }).toArray(function(error, data) {
+        if (error) {
+            manageError(res, err.message, "Failed to get contacts.");
+        } else {
+            res.status(200).json(data);
+        }
+    });
+});
+
+//Get commande a livrer
+app.get("/api/commande/livrer/:livreur", function(req, res) {
+    var id = req.params.livreur;
+    var ObjectId = require('mongodb').ObjectID;
+    database.collection('commande').find({ livreur: id ,  livrer: false }).toArray(function(error, data) {
+        if (error) {
+            manageError(res, err.message, "Failed to get contacts.");
+        } else {
+            res.status(200).json(data);
+        }
+    });
+});
+
+//Livrer
+app.get("/api/livrer/:id", function(req, res) {
+    var ObjectId = require('mongodb').ObjectID;
+    database.collection('commande').findOneAndUpdate({ _id: new ObjectID(req.params.id) }, { $set: { livrer : true } }), (function(error, data) {
+        if (error) {
+            manageError(res, err.message, "Failed to get contacts.");
+        } else {
+            res.status(200).json({ "message": "compte valider" });
+        }
+    });
+});
+
+
+//Get all user
+app.get("/api/user", function(req, res) {
+    database.collection('user').find({}).toArray(function(error, data) {
+        if (error) {
+            manageError(res, err.message, "Failed to get contacts.");
+        } else {
+            res.status(200).json(data);
+        }
+    });
+});
+
+//Delete user
+app.delete("/api/user/:id", function(req, res) {
+    if (req.params.id.length > 24 || req.params.id.length < 24) {
+        manageError(res, "Invalid product id", "ID must be a single String of 12 bytes or a string of 24 hex characters.", 400);
+    } else {
+        database.collection('user').deleteOne({ _id: new ObjectID(req.params.id) }, function(err, result) {
+            if (err) {
+                manageError(res, err.message, "Failed to delete product.");
+            } else {
+                res.status(200).json(req.params.id);
+            }
+        });
+    }
+});
+
+
+//creer token
+function createToken(mail) {
+    var salted = mail + Date.now();
+    var token = sha1(salted);
+    return token;
+
+}
+
+//delete token
+app.delete("/api/user/token/:id", function(req, res) {
+    if (req.params.id.length > 24 || req.params.id.length < 24) {
+        manageError(res, "Invalid product id", "ID must be a single String of 12 bytes or a string of 24 hex characters.", 400);
+    } else {
+        database.collection("token").deleteOne({ id_user: new ObjectID(req.params.id) }, function(err, result) {
+            if (err) {
+                manageError(res, err.message, "Failed to delete product.");
+            } else {
+                res.status(200).json(req.params.id);
+            }
+        });
+    }
+});
+
+//Login
+app.post("/api/user/login", function(req, res) {
+    var user = req.body;
+    var mpd = sha1(user.mdp);
+    database.collection('user').findOne({ email: user.email, mdp: mpd, valide: true })
+        .then(result => {
+
+            if (result != null) {
+                var id_user = result["_id"];
+                var noms = result["nom"] + " " + result["prenom"];
+                var profil = result["profil"];
+                var token = createToken(user.mail);
+                var now = new Date().getTime();
+                var expire = (10 * 60) * 1000;
+
+                database.collection('token').deleteMany({ id_user: new ObjectID(id_user) }, function(err, result) {
+                    if (err) {
+                        manageError(res, err.message, "Failed to delete product.");
+                    } else {
+
+                    }
+                });
+
+                database.collection('token').insertOne({ id_user: id_user, token: token, expire: expire, date: now }, function(err, doc) {
+                    if (err) {
+                        manageError(res, err.message, "Failed to create new product.");
+                    } else {
+                        // res.status(201).json(doc.ops[0]);
+
+                        res.status(200).json({ "status": "OK", "token": token, "id_user": id_user, "nom": noms, "email": user.email, "profil": profil });
+                    }
+                });
+
+            } else {
+                res.status(200).json({ "status": "NON", "message": "Email ou Mot de passe incorrect" });
+            }
+
+
+        })
+});
+
+
+//Inscription
+app.post("/api/user", function(req, res) {
+    var user = req.body;
+    user.mdp = sha1(user.mdp);
+    if (!user.nom) {
+        manageError(res, "Nom invalide", "Name is mandatory.", 400);
+    } else if (!user.email) {
+        manageError(res, "Email invalide", "Email is mandatory.", 400);
+    } else if (!user.mdp) {
+        manageError(res, "Mot de passe invalide", "Mdp is mandatory.", 400);
+    } else {
+        database.collection('user').insertOne(user, function(err, doc) {
+            if (err) {
+                manageError(res, err.message, "Failed to create new account.");
+            } else {
+                res.status(201).json(doc.ops[0]);
+            }
+        });
+    }
+});
+
+//send mail
+app.post("/api/user/sendmail", function(req, res) {
+    var user = req.body;
+    sendMail(user, info => {
+        console.log('mail envoyer avec succes');
+        res.send(info);
+    });
+});
+
+async function sendMail(user, callback) {
+    var transporter = nodemailer.createTransport({
+        host: "smtp.gmail.com",
+        port: 587,
+        secure: false,
+        auth: {
+            user: 'ekaly.resto@gmail.com',
+            pass: 'E-kaly#1'
+        }
+    });
+    let mailOptions = {
+        from: 'shajaniri@gmail.com',
+        to: user.email,
+        subject: "Validation compte E-kaly",
+        html: '<h1><a href="https://m1p9mean-sitraka.herokuapp.com/' + user.email + '">Valider compte</a></h1></a><br><h3>merci de nous avoir rejoint</h3>'
+    };
+
+    let info = await transporter.sendMail(mailOptions);
+
+    callback(info);
+}
+
+//Valider compte
+app.get("/api/user/valider/:email", function(req, res) {
+    var mail = req.params.email;
+    //var plat = req.body;
+    // console.log(id);
+    database.collection('user').findOneAndUpdate({ email: mail }, { $set: { valide: true } }), (function(error, data) {
+        if (error) {
+            manageError(res, err.message, "Failed to get contacts.");
+        } else {
+            res.status(200).json({ "message": "compte valider" });
+        }
+    });
+});
+
+//Check mail exist
+app.get('/api/user/:email', (req, res) => {
+    var mail = req.params.email;
+    var ObjectId = require('mongodb').ObjectID;
+    database.collection('user').findOne({ email: mail })
+        .then(result => {
+            res.status(200).json(result);
+        });
+});
+
+
+
+
+// Errors handler.
+function manageError(res, reason, message, code) {
+    console.log("Error: " + reason);
+    res.status(code || 500).json({ "error": message });
+}
